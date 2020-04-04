6a1cae2bfe913c30e92b449b397c77ea3212ed4c
dd <dd@tribepayments.com>
2020-04-04 14:20:42 +0300

    init angular js

--- environment/images/nodejs/bootstrap.sh
+++ environment/images/nodejs/bootstrap.sh
@@ -5,9 +5,11 @@ set -x
 #Updating repositories
 apt-get update -y
 
-
 #Install additional packages
 apt-get install curl nano apt-utils net-tools less dialog -y
+
+###Install GIT client
+apt-get install git -y
 
 # Using Debian, as root
 curl -sL https://deb.nodesource.com/setup_10.x | bash -
